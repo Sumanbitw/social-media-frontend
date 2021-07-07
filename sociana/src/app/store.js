82bda623f92823c8c4f74e270b8b662ec2316af5
Sumanbitw <007sumanghosh@gmail.com>
2021-07-07 20:21:14 +0530

    updated store

--- sociana/src/app/store.js
+++ sociana/src/app/store.js
@@ -1,8 +1,38 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/user/userSlice';
+import feedReducer from "../features/Feed/feedSlice"
+import profileReducer from "../features/Profile/profileSlice"
+// import storage from 'redux-persist/lib/storage';
+// import { combineReducers } from 'redux';
+// import { persistReducer } from 'redux-persist';
+// import thunk from 'redux-thunk';
+
+
+// const reducers = combineReducers({
+//     user: userReducer,
+//     feed : feedReducer,
+//     profile : profileReducer
+// });
+
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+// };
+
+// const persistedReducer = persistReducer(persistConfig, reducers);
+
+// export const store = configureStore({
+//   reducer: persistedReducer,
+//   devTools: process.env.NODE_ENV !== 'production',
+//   middleware: [thunk],
+// });
+
+// export default store;
 
 export const store = configureStore({
   reducer: {
-    user: userReducer
+    user: userReducer,
+    feed : feedReducer,
+    profile : profileReducer
   },
 });
