0b2de928676460f8b1f65ea08d8ccf8ad6306aa8
Sumanbitw <007sumanghosh@gmail.com>
2021-07-07 14:14:47 +0530

    feat : implementing feed component

---
+++ sociana/src/features/Feed/Feed.jsx
@@ -0,0 +1,147 @@
+import { Box } from '@chakra-ui/react'
+import { Avatar } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
+import { Textarea, Text } from "@chakra-ui/react"
+import { Button } from "@chakra-ui/react"
+import { FaImages, FaVideo } from 'react-icons/fa';
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { addNewPost, fetchTimelinePost, fetchUserFriends, fetchUserPost, likesUpdate } from './feedSlice'
+import { fetchUser } from '../user/userSlice'
+import { Link } from "react-router-dom"
+
+function Feed() {
+    const user = useSelector(state => state.user)
+    const feed = useSelector(state => state.feed)
+    const [postContent, setPostContent] = useState("")
+    const dispatch = useDispatch()
+
+    // console.log(user.user[0]._id)
+    // console.log(feed.posts)
+
+    useEffect(() => {
+        if(user.userLoggedIn !== null){
+            (async function(){
+                try{
+                    const result = await dispatch(fetchTimelinePost(user.user[0]._id))
+                    //  await dispatch(fetchUserPost(user?.userLoggedIn?.name)) 
+                    // await dispatch(fetchUserFriends())
+                   console.log(result)
+                }catch(error){
+                    console.log(error)
+                }
+            })()
+        }
+        return () => {}
+    },[dispatch, user.userLoggedIn, user.user])
+
+    const handleNewPost = (event) => {
+        setPostContent(event.target.value)
+    }
+
+    const validatePost = (postContent) => {
+        if(postContent === "") {
+            return false
+        }
+        return true
+    }
+
+    const handlePost = () => {
+        if(!validatePost(postContent)){
+            console.log("Cannot post empty post")
+            return;
+        }
+        dispatch(addNewPost(
+            { post : 
+                { 
+                user : user.user[0]._id, 
+                text : postContent, 
+                image : "", 
+                likes : [] 
+            }
+        }
+        ))
+        setPostContent("")
+    }
+
+    const handleLikes = (postId) => {
+        console.log(postId)
+        dispatch(likesUpdate(
+            {
+                post : 
+                {
+                    postId : postId,
+                    user : user.user[0]._id
+                }
+            }
+        ))
+    }
+
+
+    return (
+        <>
+        <Box width="full">
+            <Flex>
+                <Link to={`/profile/${user?.user[0]?.name}`}>
+                <Avatar
+                m={5}
+                name={user.user[0].name}
+                src={user.user[0].profileImage}
+                />
+                </Link>
+                <Textarea
+                mt={5}
+                mr={2}
+                size="sm"
+                placeholder="What's your thoughts for today ?"
+                value={postContent}
+                onChange={handleNewPost}
+                />
+            </Flex>
+
+            <Flex>
+                <Button ml={5} mt={3}>
+                    <FaImages/>
+                    <Text ml={2}>Images</Text>
+                </Button>
+
+                <Button ml={5} mt={3}>
+                    <FaVideo/>
+                    <Text ml={2}>Videos</Text>
+                </Button>
+
+                <Button ml={5} mt={3} onClick={handlePost}>
+                    Post
+                </Button>
+            </Flex>
+            {feed && feed.posts.map(post => (
+                // console.log(post)
+                <>
+                <Box 
+                p="6" 
+                h={64} 
+                mt="5" 
+                px="8" 
+                py="9" 
+                d='flex' 
+                flexDirection="column" 
+                justifyContent="space-between" 
+                boxShadow="base"
+                key={post._id}
+                >
+                    <Text>{post?.text}</Text>
+                    <Button 
+                    w={32}
+                    onClick={() => handleLikes(post._id)}
+                    >
+                        {post?.likes?.length} likes
+                    </Button>
+                </Box>
+                </>
+            ))}
+        </Box>
+        </>
+    )
+}
+
+export default Feed
---
+++ sociana/src/features/Feed/feedSlice.js
@@ -0,0 +1,167 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from "axios"
+import { API_URL } from '../../utility'
+
+export const fetchTimelinePost = createAsyncThunk('/feed/timelinePost', async(userId) => {
+    // console.log(userId)
+    const response = await axios.get(`${API_URL}/posts/timeline/${userId}`)
+    // console.log(response)
+    return response.data
+})
+
+export const fetchUserPost = createAsyncThunk('feed/userPost', async (name) => {
+    const response = await axios.get(`${API_URL}/posts/profile/${name}`)
+    // console.log(response)
+    return response.data
+})
+
+export const fetchUserFriends = createAsyncThunk('feed/userFriends', async(userId) => {
+    const response = await axios.get(`${API_URL}/user/friends/${userId}`)
+    console.log(response)
+    return response.data
+}) 
+
+export const addNewPost = createAsyncThunk('feed/newPost', async(post) => {
+    const response = await axios.post(`${API_URL}/posts`, post['post'])
+    // console.log(response)
+    return response.data
+})
+
+export const likesUpdate = createAsyncThunk('feed/likes', async(post) => {
+    // console.log(post.post.user)
+    const response = await axios.post(`${API_URL}/posts/${post.post.postId}/like`,
+    {
+        user : post.post.user
+    })
+    return response.data
+})
+
+export const fetchUserFeed = createAsyncThunk('user/fetchUser', 
+    async (_, { getState }) => {
+        const response = await axios.get(`${API_URL}/user`)
+        console.log(response)
+        return response.data
+    })
+
+export const feedSlice = createSlice({
+    name: 'post',
+    initialState: { 
+        friends : [],
+        posts : [], 
+        user : [],
+        status : 'idle',
+        error : null 
+      },
+    reducers: {
+  
+    },
+  
+    extraReducers: {
+       [fetchUserPost.pending]: (state) => {
+           state.status = 'loading'
+       },
+       [fetchUserPost.fulfilled]: (state, action) => {   
+        //    console.log(action.payload)  
+           state.status = 'succeeded';       
+           state.posts = action.payload;
+          
+       },
+       [fetchUserPost.rejected]: (state, action) => {            
+            state.status = 'error';
+            state.error = action.error.message;
+       },
+      
+       [fetchTimelinePost.pending]: (state) => {
+            state.status = 'loading'
+        },
+
+       [fetchTimelinePost.fulfilled]: (state, action) => {   
+            // console.log(action.payload) 
+            state.status = 'succeeded';        
+            state.posts = action.payload;
+        
+        },
+       [fetchTimelinePost.rejected]: (state, action) => {            
+            state.status = 'error';
+            state.error = action.error.message;
+        },
+       [fetchUserFeed.pending]: (state, action) => {
+            state.status = 'loading';
+        },
+       [fetchUserFeed.fulfilled]: (state, action) => { 
+            state.status = 'succeeded';
+            // console.log(action.payload)            
+            state.user = action.payload.userLoggedIn;
+        },
+       [fetchUserFeed.rejected]: (state, action) => {
+            // console.log("rejected", state, action);
+            state.status = 'error';
+            state.error = action.error.message;
+        }, 
+
+       [addNewPost.pending] : (state,action) => {
+            state.status = "loading"
+        },
+
+       [addNewPost.fulfilled] : (state,action) => {
+            // console.log(action.payload)
+            state.status = "succeeded"
+            state.posts.unshift(action.payload)
+    },
+
+       [addNewPost.rejected] : (state,action) => {
+            state.status = "failed"
+            state.error = action.payload.message
+        },
+
+        [likesUpdate.pending] : (state) => {
+            state.status = "loading"
+        },
+
+        [likesUpdate.fulfilled] : (state, action) => {
+            state.status = "succeeded";
+            // console.log(action.payload.updatedPost, " payload")
+            const { _id, likes, user } = action.payload.updatedPost
+            console.log("lineNumber 123",_id, likes, user)
+
+            const isPostLiked = likes.includes(user)
+            console.log(isPostLiked)
+            
+            if(isPostLiked){
+                state.posts.map(post => 
+                    post._id === _id
+                    ? { ...post, likes : post.likes.push(user)}
+                    : post
+                    )
+            }else{
+                state.posts.map(post => 
+                    post._id === _id 
+                    ? {...post, likes : post.likes.filter(like => console.log(like))}
+                    : post
+                    )
+            }
+            
+        },
+
+        [likesUpdate.rejected] : (state, action) =>{
+            state.status = "error";
+            state.error = action.error.message
+        },
+
+        [fetchUserFriends.pending]: (state, action) => {
+            state.status = 'loading';
+        },
+        [fetchUserFriends.fulfilled]: (state, action) => { 
+            state.status = 'succeeded';            
+            state.friends = action.payload.friendList;
+        },
+        [fetchUserFriends.rejected]: (state, action) => {
+            console.log("rejected", state, action);
+            state.status = 'error';
+            state.error = action.error.message;
+        },       
+    },
+  })
+  
+  export default feedSlice.reducer
+  
